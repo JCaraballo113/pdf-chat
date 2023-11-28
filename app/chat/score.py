7fcf086f5225866efc832c773f39e32a5f3f1536
John Caraballo <jcaraballo019@gmail.com>
2023-11-28 06:30:17 -0500

    add scoring convos

--- app/chat/score.py
+++ app/chat/score.py
@@ -1,23 +1,17 @@
+from app.chat.redis import client
+
+
 def score_conversation(
     conversation_id: str, score: float, llm: str, retriever: str, memory: str
 ) -> None:
-    """
-    This function interfaces with langfuse to assign a score to a conversation, specified by its ID.
-    It creates a new langfuse score utilizing the provided llm, retriever, and memory components.
-    The details are encapsulated in JSON format and submitted along with the conversation_id and the score.
+    score = min(max(score, 0), 1)
 
-    :param conversation_id: The unique identifier for the conversation to be scored.
-    :param score: The score assigned to the conversation.
-    :param llm: The Language Model component information.
-    :param retriever: The Retriever component information.
-    :param memory: The Memory component information.
-
-    Example Usage:
-
-    score_conversation('abc123', 0.75, 'llm_info', 'retriever_info', 'memory_info')
-    """
-
-    pass
+    client.hincrby("llm_score_values", llm, score)
+    client.hincrby("llm_score_count", llm, 1)
+    client.hincrby("retriever_score_values", retriever, score)
+    client.hincrby("retriever_score_count", retriever, 1)
+    client.hincrby("memory_score_values", memory, score)
+    client.hincrby("memory_score_count", memory, 1)
 
 
 def get_scores():
